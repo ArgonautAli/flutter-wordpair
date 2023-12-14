49ff0aaf2a6ac4d254296bd03f08b62eb8df235e
pibitHaider <haider.salar@gmail.com>
2023-12-14 18:26:21 +0530

    feat: added navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(
           primarySwatch: Colors.pink,
@@ -28,20 +27,5 @@ class MyApp extends StatelessWidget {
             //   ),
             // ),
             RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Wordpair generator"),
-        ),
-        body: Text("Hello"));
   }
 }
